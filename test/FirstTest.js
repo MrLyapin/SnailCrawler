6f0937aeb192d71063482c226ef43f46e295341b
Lyapin Nick <nick.v.lyapin@gmail.com>
2020-01-06 00:47:04 +0300

    fix bug

--- test/FirstTest.js
+++ test/FirstTest.js
@@ -3,11 +3,16 @@ const fs = require('fs');
 const colors = require('colors');
 const dateformat = require('dateformat');
 
-const startURL = "https://read.dukeupress.edu/cssaame/search-results?page=1&q=&fl_SiteID=1000023";
-const links = "div.sri-title.customLink.al-title > h4 > a";
+const startURL = "http://www.tcsae.org/nygcxben/ch/reader/key_query.aspx";
+const links = "#DataGrid1  tbody  tr  td  a[target]";
+
+// const startURL = "https://read.dukeupress.edu/cssaame/search-results?page=1&q=&fl_SiteID=1000023";
+// const links = "div.sri-title.customLink.al-title > h4 > a";
 
 // const startURL = "https://www.google.com";
 // const links = "div.r > a";
+
+
 
 let currentURL = startURL;
 let stopURL = 'http://stop/';
@@ -22,73 +27,63 @@ let uniqLinksSize;
 let done = false;
 
 //stat
-let oldSize = 0;
+let qq = false;
 let parseOk = 0;
 
 
 
-describe( startURL + ' page', () => {
+describe(startURL + ' page', () => {
     it('SnailCrawler => done', () => {
 
 
 
-        browser.url(currentURL); // open start page
-        while(browser.getUrl() !== stopURL) {
 
-            previousURL = currentURL;
-            uniqLinksSize = uniqLinks.size;
+
+
+        browser.url(currentURL); // open start page
+        while (browser.getUrl() !== stopURL) { // loop
+
+            uniqLinksSize = uniqLinks.size; // start = 0;
 
             if ($(links).isExisting()) { // check element
-                //console.log("start parsing");
-
                 timer = Date.now(); // set date now
                 findElements = $$(links);
                 for (let i of findElements) {
-
-                    //console.info(i.getAttribute("href"));
                     try {
                         uniqLinks.add(i.getAttribute("href"));
-
                     }
                     catch (err) {
                         break;
                     }
-
                 }
 
-
-                timer = Date.now() - timer;
-                if (uniqLinksSize === uniqLinks.size && done === false) {
-
-				
-                    if(oldSize === uniqLinksSize) {
-                        console.log(colors.red(currentURL));
-                        console.log(colors.red("WARNING!!! the link is already in the database !!!", colors.white("please visit " + "page " + (parseOk+1))));
-                    }else{
-                        parseOk++;
-                        console.log(colors.blue(currentURL));
-                        console.log(colors.green("#" + parseOk + " page parsed. Unique links:") + uniqLinksSize + "   time to parse:".green + timer + "ms", "next page:" + (parseOk+1));
-                    }
-                    oldSize = uniqLinksSize;
-			        done = true;
+                if (uniqLinksSize != uniqLinks.size) {
+                    done = true;
                 }
 
             }
 
-                    currentURL = browser.getUrl();
-                    if(currentURL !== previousURL) {
-                        done = false;
-                    }
+            timer = Date.now() - timer;
 
-        }
+
+            if (uniqLinksSize === uniqLinks.size && done === true) {
+
+                parseOk++;
+                console.log(colors.blue(currentURL));
+                console.log(colors.green("#" + parseOk + " page parsed. Unique links:") + uniqLinksSize + "   time to parse:".green + timer + "ms", "next page:" + (parseOk + 1));
+                done = false;
+
+            };
+
+        };
 
 
 
         arrLinks = [...uniqLinks];
         text = arrLinks.join("\n");
-        let date = dateformat(Date.now(),"yyyy-mm-dd'T'HH:MM:ss" );
+        let date = dateformat(Date.now(), "yyyy-mm-dd'T'HH:MM:ss");
 
-        fs.writeFile("./link_list/"+ date + '.txt', text, (err) => {
+        fs.writeFile("./link_list/" + date + '.txt', text, (err) => {
 
             if (err) throw err;
             console.log(colors.red('Report: '), colors.green('links saved!'));
@@ -98,4 +93,3 @@ describe( startURL + ' page', () => {
     });
 
 });
-
